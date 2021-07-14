eb6ead84ce8d0162ba0e6e2beb358bc465d53d15
Chris Scheible <cgscheible@yahoo.com>
2021-07-14 18:25:10 +0800

    templates/outputs.tf - test EC2 public IP

--- templates/ec2.tf
+++ templates/ec2.tf
@@ -35,31 +35,30 @@ resource "aws_subnet" "web_subnet" {
   }
 }
 
-resource "aws_network_interface" "web" {
-  subnet_id   = aws_subnet.web_subnet.id
-  private_ips = ["172.16.10.100"]
+#resource "aws_network_interface" "web" {
+#  subnet_id   = aws_subnet.web_subnet.id
+#  private_ips = ["172.16.10.100"]
+#
+#  tags = {
+#    Name = "primary_network_interface"
+#  }
+#}
 
-  tags = {
-    Name = "primary_network_interface"
-  }
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKY1FMoHXvdkc5VgNj6sVfswURPwY5NgD5V0lhEFP1quvP90deszyryY9sb4acGPGpea/WyE4td3rRh9MQi6jrIO/Oi5KD5NF3VYYWGdAxAqoe4QNZPYEaAH4CQX6hMzye4eXiGxuoC+/wtlTDvFbHrCUtvvuEvQ1f+w+PKRr0rOC2EZUvdoA2/QV4UdGlbd1DkaWT8W819n6Jmj3VBGTaOUtqDyPJLvZKl4wDEkZhsREAb9kHnRDl1IfvQeWWVMSpQt4RvTJSQKcGy1DHu//c6E2UWZHiMyJ4oGvnZVK5QUh56hTFrvi9Qb+dA8T5Artc4oKEKthKaHgSxeOOw2Zp"
 }
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  key_name      = "debian-test"
+  key_name      = "deployer-key"
   instance_type = "t2.micro"
   monitoring    = true
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-
   tags = {
     Name = random_pet.name.id
   }
-
-  #network_interface {
-  #  network_interface_id = aws_network_interface.web.id
-  #  device_index         = 0
-  #}
 
 }
 
@@ -79,5 +78,4 @@ resource "aws_security_group" "web-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 
--- templates/outputs.tf
+++ templates/outputs.tf
@@ -5,3 +5,8 @@ output "domain-name" {
 output "application-url" {
   value = "${aws_instance.web.public_dns}/index.php"
 }
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
