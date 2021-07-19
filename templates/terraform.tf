84c26490105791744a22ead5f47218deb791e9ec
Chris Scheible <cgscheible@yahoo.com>
2021-07-19 18:17:30 +0800

    .github/workflows/tf-plan.yaml - test AWS SSO

--- templates/terraform.tf
+++ templates/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
-  backend "s3" {
-    key = "terraform-aws/terraform.tfstate"
-  }
+  #backend "s3" {
+  #  key = "terraform-aws/terraform.tfstate"
+  #}
   required_providers {
     aws = {
       source = "hashicorp/aws"
